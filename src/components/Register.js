2fda605019e32ee3a5ec7d80039c23a82aa96433
Kirill Danilychev <k3499@yandex.ru>
2021-06-24 21:37:27 +0300

    fix: правки вторая часть

--- src/components/Register.js
+++ src/components/Register.js
@@ -27,8 +27,8 @@ function Register({ handleRegister } ) {
       <h2 className="form__title">Регистрация</h2>
       <form className="form__form" onSubmit={handleSubmit}>
         <div className="form__input-box">
-          <input type="email" placeholder="Email" onChange={handleChangeEmail} className="form__input form__input_email" />
-          <input type="password" placeholder="Пароль" onChange={handleChangePass} className="form__input form__input_pass" />
+          <input type="email" placeholder="Email" value={email} onChange={handleChangeEmail} className="form__input form__input_email" />
+          <input type="password" placeholder="Пароль" value={password} onChange={handleChangePass} className="form__input form__input_pass" />
         </div>
         <button type="submit" className={(!email || !password)
           ? "form__submit form__disabled"
